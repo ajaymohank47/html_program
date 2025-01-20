bd19faf9c772f47cdceff0df24e6226914e2b8a1
unknown <ajaymohank47@gmail.com>
2025-01-20 10:52:44 +0530

    added more files

---
+++ JavaScript/string.js
@@ -0,0 +1,7 @@
+var str="Hello, Myself Shreyas Gaikwad"
+console.log(str)
+console.log(str.length);
+console.log(str.charAt(1));
+console.log(str.startsWith("Vasantdada"));
+console.log(str.endsWith("College"));
+console.log()
