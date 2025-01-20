d13640caee896ee23f40825ad06958f83362b326
unknown <ajaymohank47@gmail.com>
2025-01-20 14:28:11 +0530

    added more files to javaScript

---
+++ JavaScript/arrow.js
@@ -0,0 +1,15 @@
+const displayInfo=()=>{
+
+    console.log("This is an arrow method");
+
+}
+
+const getInfo=()=>{
+
+    console.log("This is a get Info function");
+
+}
+
+displayInfo();
+
+getInfo();
