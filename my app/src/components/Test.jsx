30ed8f5d951e0af83d45c938e07a28cf726d7dfc
ajaymohank47 <ajaymohank47@gmail.com>
2025-01-21 20:16:20 +0530

    Delete my app directory

--- my app/src/components/Test.jsx
+++
@@ -1,8 +0,0 @@
-function App(){
-    return (
-      <div>
-       <h2>This is Test.jsx</h2>
-      </div>
-    )
-  }
-  export default App;
--- my app/src/components/Test1.jsx
+++
@@ -1,8 +0,0 @@
-function App(){
-    return (
-      <div>
-       <h2>This is Test1.jsx</h2>
-      </div>
-    )
-  }
-  export default App;
