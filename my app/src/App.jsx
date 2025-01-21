7e41d88619fad32ba74db27b91ab9efa20c7096e
unknown <ajaymohank47@gmail.com>
2025-01-21 11:34:26 +0530

    Initial commit

---
+++ my app/src/App.jsx
@@ -0,0 +1,11 @@
+import Test from './components/Test'
+import Test1 from './components/Test1'
+function App(){
+    return (
+      <div>
+       <Test></Test>
+       <Test1></Test1>
+      </div>
+    )
+  }
+  export default App;
---
+++ my app/src/components/Test1.jsx
@@ -0,0 +1,8 @@
+function App(){
+    return (
+      <div>
+       <h2>This is Test1.jsx</h2>
+      </div>
+    )
+  }
+  export default App;
